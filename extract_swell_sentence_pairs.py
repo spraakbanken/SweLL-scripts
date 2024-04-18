3c56e509f5c347dbdd8b2bd4101e4faf9bce6230
Arianna Masciolini <uzkamascio@gmail.com>
2024-04-18 22:03:01 +0200

    retain metadata + conllu output

--- extract_swell_sentence_pairs.py
+++ extract_swell_sentence_pairs.py
@@ -1,9 +1,9 @@
 ### This code can be used to extract pairs of sentences from the SweLL-gold corpus sources (originals) and targets (correction hypotheses) where the two have the same length.
 
 import xml.etree.ElementTree as ET
-import pandas as pd
-import os
 import argparse
+import csv
+import conllu
 
 #######################################################################
 
@@ -63,24 +63,27 @@ def get_sent_dict(essays: list):
     essay_dict = {}
     essay_sents = []
     for essay in essays:
-        essay_id = essay[0][2]['essay_id']
+        metadata = essay[0][2] 
+        essay_id = metadata['essay_id']
         sent = []
-        sent_err_labels = []
-        for elem in essay[1:]:  # without metadata
-            if elem[0] == 'link':
+        for (cat,word,info) in essay[1:]:  # without metadata
+            if cat == 'link':
                 continue
-            elif elem[0] == 'sentence':
+            elif cat == 'sentence':
                 if len(sent) > 0:
-                    essay_sents.append((sent, sent_err_labels))
+                    essay_sents.append(sent)
                     sent = []
-                    sent_err_labels = []
-            else:
-                sent.append(elem[1])
-                if "correction_label" in elem[2]:
-                    sent_err_labels.append(elem[2]["correction_label"])
+            else: 
+                if '␤' not in word and essay_id not in word:
+                    if 'A-' in word or 'B-' in word or 'C-' in word or 'D-' in word:
+                        try:
+                            word = placeholder_map[word[2:]]
+                        except KeyError:
+                            pass
+                    sent.append((word, info["correction_label"] if "correction_label" in info else "_"))
     
-        essay_sents.append((sent, sent_err_labels))
-        essay_dict[essay_id] = essay_sents
+        essay_sents.append(sent)
+        essay_dict[essay_id] = {"metadata": metadata, "sentences": essay_sents}
         essay_sents = []
         
         
@@ -97,75 +100,75 @@ def pair_up(source_dict: dict, target_dict: dict):
         A list tuples of (original sentence, target sentence, essay ID).
     '''
     paired_up = []
-    for k, v in source_dict.items():
-        if len(v) == len(target_dict[k]):
-            for i, (sent, labels) in enumerate(v):
-                paired_up.append((sent, target_dict[k][i][0], k, labels))
+    for essay_id, val in source_dict.items():
+        metadata = val["metadata"]
+        assert metadata == target_dict[essay_id]["metadata"]
+        sentences = val["sentences"]
+        if len(sentences) == len(target_dict[essay_id]["sentences"]):
+            for i, sent in enumerate(sentences):
+                paired_up.append({
+                    "original": sent, 
+                    "target": target_dict[essay_id]["sentences"][i], 
+                    "metadata": metadata})
     return paired_up
 
-def replace_placeholders(sentence: list, placeholder_map: dict, essay_id: str):
-    '''A function that replaces anonymized tokens with some mapping, and removes the essay ID and newline from the essay.
-    
-    Args:
-        sentence (list): A sentence represented as a list of tokens.
-        placeholder_map (dict): A dictionary mapping possible placeholders to other tokens.
-        essay_id (str): The ID of the essay the sentence is from.
-        
-    Returns:
-        A list tuples of (original sentence, target sentence, essay ID).
-    '''
-    for i, word in enumerate(sentence):
-        if 'A-' in word or 'B-' in word or 'C-' in word or 'D-' in word:
-            try:
-                sentence[i] = placeholder_map[word[2:]]
-            except KeyError:
-                continue
-        sentence = [word for word in sentence if '␤' not in word and essay_id not in word]
-    return sentence
+def tokenlist(word_label_pairs, metadata):
+    tokens = []
+    for i, (word,label) in enumerate(word_label_pairs):
+        tokens.append(conllu.Token(
+            id=i + 1, 
+            form=word, 
+            lemma="_",
+            upos="_",
+            xpos="_",
+            feats="_",
+            head="_",
+            deprel="_",
+            deps="_",
+            misc=label))
+    return conllu.TokenList(tokens=tokens, metadata=metadata)
 
-def create_df(paired_up, placeholder_map):
-    '''A function that creates a DataFrame out of the paired up sentences and replaces the placeholder tokens with naive surrogates.
-    
-    Args:
-        paired_up (list): A list of paired-up sentences.
-        placeholder_map (dict): A dictionary mapping possible placeholders to other tokens.
-        
-    Returns:
-        A pandas DataFrame with the original sentence, target sentence, and essay ID with undesirable elements removed.
-    '''
-    sentence_pairs = []
-    for pair in paired_up:
-        essay_id = pair[2]
-        labels = pair[3]
-        sent1 = ' '.join(replace_placeholders(pair[0], placeholder_map, essay_id))
-        sent2 = ' '.join(replace_placeholders(pair[1], placeholder_map, essay_id))
-        
-        sentence_pairs.append((sent1, sent2, essay_id, ",".join(labels)))
-
-    df = pd.DataFrame(sentence_pairs)
-    df.columns = ['Source sentence', 'Target sentence', 'Essay ID', 'Correction labels']
-    df.set_index("Source sentence", inplace=True)
-    return df
-            
 if __name__ == "__main__":
+    formats = ["tsv", "conllu"]
     parser = argparse.ArgumentParser()
 
     parser.add_argument('source', help='The path to the sourceSweLL.xml file')
     parser.add_argument('target', help='The path to the targetSweLL.xml file')
-    parser.add_argument('--filename', required=False, default='aligned_sentences.tsv', help='The name for the output file.')
+    parser.add_argument('--format', required=False, default="tsv", help='output format (tab-separated or CONLL-U)', type=str, choices=formats)
+    parser.add_argument('--outfile', required=False, default="swell_sent_pairs.tsv", type=str, help='The name for the output file. If outputting CoNLL-U "org-" resp. "trg-" are added to the corresponding filenames')
     
     args = parser.parse_args()
      
     source_essays = get_essays(args.source)
     target_essays = get_essays(args.target)
+    output_format = args.format
+    if not output_format in formats:
+        print("invalid format, please select 'conllu' or 'tsv'")
+        exit(-1)
     
     source_dict = get_sent_dict(source_essays)
     target_dict = get_sent_dict(target_essays)
 
-    paired_up = pair_up(source_dict, target_dict)
+    pairs = pair_up(source_dict, target_dict)
     
-    df = create_df(paired_up, placeholder_map)
-    
-    # save as a TSV file
-    df.to_csv(args.filename, sep='\t')
-    
+    if output_format == "tsv":
+        with open(args.outfile, "w") as outfile:
+            for (i,pair) in enumerate(pairs):
+                
+                org_sent = " ".join([token for (token,_) in pair["original"]])
+                trg_sent = " ".join([token for (token,_) in pair["target"]])
+                labels = ",".join([label for (_,label) in pair["original"] if label != "_"])
+                csv_row = pair["metadata"] | {"original sentence": org_sent, "corrected sentence": trg_sent, "correction labels": labels}
+                writer = csv.DictWriter(outfile, csv_row.keys(), delimiter="\t")
+                if i == 0: writer.writeheader()
+                writer.writerow(csv_row)     
+
+    else: # conllu
+        with open("org-" + args.outfile, "w") as org_outfile, open("trg-" + args.outfile, "w") as trg_outfile:
+            for pair in pairs:
+                metadata = pair["metadata"]
+                org_sent = tokenlist(pair["original"], metadata)
+                trg_sent = tokenlist(pair["target"], metadata)
+                org_outfile.write(org_sent.serialize())
+                trg_outfile.write(trg_sent.serialize())
+            
